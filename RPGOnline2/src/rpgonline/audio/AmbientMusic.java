1f124f4086ef2f8e3828f2d725128bfa786c0ec8
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2019-07-26 12:41:27 +0100

    Documented AmbientMusic #22

--- RPGOnline2/src/rpgonline/audio/AmbientMusic.java
+++ RPGOnline2/src/rpgonline/audio/AmbientMusic.java
@@ -1,16 +1,45 @@
 package rpgonline.audio;
 
 /**
+ * <p>
  * A class for storing Ambient Music.
+ * </p>
+ * <p>
+ * Ambient music will loop until changed (using cross-fading). Through the use
+ * of groups individual parts of music can be changed to perform vertical
+ * mixing. Horizontal mixing currently has to be done using
+ * {@link rpgonline.audio.AudioManager#setMusic(AmbientMusic)}
+ * </p>
+ * 
  * @author Tomas
- *
  */
 public class AmbientMusic {
+	/**
+	 * The set of sounds associated with this piece of music.
+	 */
 	private final String[] sounds;
+	/**
+	 * The sound groups associated with the sounds.
+	 */
 	private final String[] groups;
+	/**
+	 * The volumes associated with each sound.
+	 */
 	private final float[] volumes;
+	/**
+	 * An internally-used list of references to currently-playing audio.
+	 * 
+	 * @see rpgonline.audio.AudioManager#setMusic(AmbientMusic)
+	 */
 	final String[] refs;
 
+	/**
+	 * Constructs a new piece of ambient music using 3 arrays of equal length.
+	 * 
+	 * @param sounds  The set of sounds associated with this piece of music.
+	 * @param groups  The sound groups associated with the each sound.
+	 * @param volumes The volumes associated with each sound.
+	 */
 	public AmbientMusic(String[] sounds, String[] groups, float[] volumes) {
 		super();
 		this.sounds = sounds;
@@ -18,27 +47,54 @@ public class AmbientMusic {
 		this.groups = groups;
 		this.volumes = volumes;
 	}
-	
+
+	/**
+	 * Constructs a new piece of ambient music from one audio track on one sound group at the specified volume.
+	 * @param sound The sound to loop.
+	 * @param group The sound group associated with the sound.
+	 * @param volume The volume to play the music at.
+	 */
 	public AmbientMusic(String sound, String group, float volume) {
-		this(new String[] { sound }, new String[] { group }, new float[] {volume});
+		this(new String[] { sound }, new String[] { group }, new float[] { volume });
 	}
-	
+
+	/**
+	 * Constructs a new piece of ambient music from one audio track at a specified volume.
+	 * @param sound The sound to loop.
+	 * @param volume The volume to play the music at.
+	 */
 	public AmbientMusic(String sound, float volume) {
 		this(sound, "music", volume);
 	}
-	
+
+	/**
+	 * Constructs a new piece of ambient music from one audio track at full volume.
+	 * @param sound The sound to loop.
+	 */
 	public AmbientMusic(String sound) {
 		this(sound, 1f);
 	}
 
+	/**
+	 * Gets the list of sounds in this music.
+	 * @return The array of sounds used by this music.
+	 */
 	public String[] getSounds() {
 		return sounds;
 	}
 
+	/**
+	 * Gets the list of sound groups. in this music.
+	 * @return The array of sound group IDs used by this music.
+	 */
 	public String[] getGroups() {
 		return groups;
 	}
 
+	/**
+	 * Gets the list of volumes in this music.
+	 * @return The array of volumes used by this music.
+	 */
 	public float[] getVolumes() {
 		return volumes;
 	}
