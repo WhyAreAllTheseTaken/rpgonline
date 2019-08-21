694399aaff74aec53dcdb40a385705df4b9cb0ab
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2019-08-21 19:14:43 +0100

    Documented tag doc #49

--- RPGOnline2/src/rpgonline/abt/TagDoc.java
+++ RPGOnline2/src/rpgonline/abt/TagDoc.java
@@ -10,8 +10,21 @@ import org.newdawn.slick.util.Log;
 
 import rpgonline.debug.Debugger;
 
+/**
+ * A representation of an ABT file.
+ * @author Tomas
+ */
 public class TagDoc {
+	/**
+	 * The most recent ABT version.
+	 */
 	public static final short LATEST_VERSION = 1;
+	/**
+	 * Construct a new tag doc.
+	 * @param version The version format of the tag doc.
+	 * @param app The app name of the tag doc.
+	 * @param tags The root tag group used by this tag doc.
+	 */
 	public TagDoc(short version, String app, TagGroup tags) {
 		super();
 		this.version = version;
@@ -19,33 +32,84 @@ public class TagDoc {
 		this.tags = tags;
 	}
 	
+	/**
+	 * Construct a new tag doc.
+	 * @param app The app name of the tag doc.
+	 * @param tags The root tag group used by this tag doc.
+	 */
 	public TagDoc(String app, TagGroup tags) {
 		this(LATEST_VERSION, app, tags);
 	}
 	
+	/**
+	 * The ABT version.
+	 */
 	private short version;
+	/**
+	 * The app format.
+	 */
 	private String app;
+	/**
+	 * The root tag group.
+	 */
 	private TagGroup tags;
 	
+	/**
+	 * Gets the ABT version of this document.
+	 * @return A short value.
+	 */
 	public short getVersion() {
 		return version;
 	}
+	/**
+	 * Sets the ABT version of this document.
+	 * @param version A supported version value.
+	 */
 	public void setVersion(short version) {
+		if (!(version >= 0 && version <= TagDoc.LATEST_VERSION)) {
+			throw new IllegalArgumentException("Unsupported tag version: " + version);
+		}
 		this.version = version;
 	}
+	/**
+	 * Gets the app ID for this document.
+	 * @return A string.
+	 */
 	public String getApp() {
 		return app;
 	}
+	/**
+	 * Sets the app ID for this document.
+	 * @return A string of 255 characters or less.
+	 */
 	public void setApp(String app) {
+		if (app.length() > 255) {
+			throw new IllegalArgumentException("app ID must be less than or equal to 255 characters.");
+		}
 		this.app = app;
 	}
+	/**
+	 * Gets the root tag of this document.
+	 * @return A tag group.
+	 */
 	public TagGroup getTags() {
 		return tags;
 	}
+	/**
+	 * Sets the root tag of this document.
+	 * @param tags A tag group.
+	 */
 	public void setTags(TagGroup tags) {
 		this.tags = tags;
 	}
 	
+	/**
+	 * Reads a tag document from an ABT file.
+	 * @param in An input stream.
+	 * @param app The app ID to check against or {@code null} to disable checking.
+	 * @return A tag doc object.
+	 * @throws IOException If an error occurs loading data.
+	 */
 	public static TagDoc read(InputStream in, String app) throws IOException {
 		Debugger.start("abt-decode");
 		try {
@@ -99,6 +163,11 @@ public class TagDoc {
 		}
 	}
 	
+	/**
+	 * Writes this document to a stream.
+	 * @param out The stream to write to.
+	 * @throws IOException If an error occurs writing data.
+	 */
 	public void write(OutputStream out) throws IOException {
 		Debugger.start("abt-encode");
 		try {
@@ -121,6 +190,12 @@ public class TagDoc {
 		Debugger.stop("abt-encode");
 	}
 	
+	/**
+	 * Writes a tag.
+	 * @param out The output stream to write to.
+	 * @param t The tag to write.
+	 * @throws IOException If an error occurs writing data.
+	 */
 	@SuppressWarnings("deprecation")
 	public void writeTag(ABTDataOutputStream out, Tag t) throws IOException {
 		out.writeByte(t.getType());
@@ -301,6 +376,13 @@ public class TagDoc {
 		}
 	}
 	
+	/**
+	 * Reads a tag group from a stream.
+	 * @param name The name of the tag group.
+	 * @param in The input stream to read from.
+	 * @return A tag group.
+	 * @throws IOException If an error occurs reading data.
+	 */
 	public static TagGroup readTagGroup(String name, ABTDataInputStream in) throws IOException {
 		// Item count. Not needed by this parser.
 		in.readLong();
@@ -319,6 +401,13 @@ public class TagDoc {
 		
 		return g;
 	}
+	/**
+	 * Reads a tag from a stream.
+	 * @param id The id of the tag.
+	 * @param in The input stream to read from.
+	 * @return A tag/
+	 * @throws IOException If an error occurs reading data.
+	 */
 	@SuppressWarnings("deprecation")
 	public static Tag readTag(byte id, ABTDataInputStream in) throws IOException {
 		byte[] buffer;
