d561c281eae5577ed66cc69fc2db2d7573b7fb95
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2019-09-01 16:13:06 +0100

    Documented debug api. #44

--- RPGOnline2/src/rpgonline/debug/DebugFrame.java
+++ RPGOnline2/src/rpgonline/debug/DebugFrame.java
@@ -5,13 +5,31 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
+/**
+ * A single frame of debug information.
+ * @author Tomas
+ */
 public final class DebugFrame {
+	/**
+	 * The start times for currently running tasks.
+	 */
 	private final Map<String, Long> start = new HashMap<>();
+	/**
+	 * The total times for completed tasks.
+	 */
 	private final Map<String, Long> times = new HashMap<>();
 	
+	/**
+	 * Starts a task.
+	 * @param id The task ID.
+	 */
 	public void start(String id) {
 		start.put(id, System.nanoTime());
 	}
+	/**
+	 * Stops a task.
+	 * @param id The task ID.
+	 */
 	public void stop(String id) {
 		Long t = times.get(id);
 		
@@ -21,6 +39,10 @@ public final class DebugFrame {
 		times.put(id, t + (System.nanoTime() - start.get(id)));
 	}
 	
+	/**
+	 * Gets a set of all times in this frame.
+	 * @return A set object.
+	 */
 	public Set<Entry<String, Long>> getTimes() {
 		return times.entrySet();
 	}
--- RPGOnline2/src/rpgonline/debug/Debugger.java
+++ RPGOnline2/src/rpgonline/debug/Debugger.java
@@ -5,6 +5,11 @@ import java.util.Map;
 
 import rpgonline.RPGConfig;
 
+/**
+ * The primary class for the debugging API.
+ * @author Tomas
+ * @see rpgonline.RPGGame
+ */
 public final class Debugger {
 	/**
 	 Prevent instantiation
@@ -13,9 +18,18 @@ public final class Debugger {
 		
 	}
 	
+	/**
+	 * The map of debug frames.
+	 */
 	private static final Map<Thread, DebugFrame> debug = new HashMap<>();
+	/**
+	 * The thread used for rendering.
+	 */
 	private static Thread renderThread;
 	
+	/**
+	 * Starts the current debug frame. This will overright existing debug frames on this thread.
+	 */
 	public static final void start() {
 		if (RPGConfig.isDebug()) {
 			debug.put(Thread.currentThread(), new DebugFrame());
@@ -23,14 +37,24 @@ public final class Debugger {
 		}
 	}
 	
+	/**
+	 * Initialises this thread as the thread for render operations.
+	 */
 	public static final void initRender() {
 		if (RPGConfig.isDebug()) renderThread = Thread.currentThread();
 	}
 	
+	/**
+	 * Stops the current debug frame.
+	 */
 	public static final void stop() {
 		if (RPGConfig.isDebug()) stop("total");
 	}
 	
+	/**
+	 * Starts a task within a debug frame. This can be repeated multiple times per frame for events that occur many times.
+	 * @param id The ID of the task.
+	 */
 	public static final void start(String id) {
 		if (RPGConfig.isDebug()) {
 			DebugFrame f = debug.get(Thread.currentThread());
@@ -38,6 +62,10 @@ public final class Debugger {
 		}
 	}
 	
+	/**
+	 * Stops a task within a debug frame. This can be repeated multiple times per frame for events that occur many times.
+	 * @param id The ID of the task.
+	 */
 	public static final void stop(String id) {
 		if (RPGConfig.isDebug()) {
 			DebugFrame f = debug.get(Thread.currentThread());
@@ -45,14 +73,27 @@ public final class Debugger {
 		}
 	}
 	
+	/**
+	 * Gets the most recent debug frame from the render thread.
+	 * @return A possibly-incomplete debug frame object.
+	 */
 	public static final DebugFrame getRenderFrame() {
 		return debug.get(renderThread);
 	}
 	
+	/**
+	 * Gets the most recent debug frame from the specified thread.
+	 * @param t A non-null thread object.
+	 * @return A possibly-incomplete debug frame object.
+	 */
 	public static final DebugFrame getFrame(Thread t) {
 		return debug.get(t);
 	}
 	
+	/**
+	 * Gets the most recent debug frame from this thread.
+	 * @return A possibly-incomplete debug frame object.
+	 */
 	public static final DebugFrame getFrame() {
 		return getFrame(Thread.currentThread());
 	}
