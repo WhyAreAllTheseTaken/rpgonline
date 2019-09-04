302eb24437c483e41e688bd04945e884768e326a
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2019-09-04 19:40:52 +0100

    Documented particles #37

--- RPGOnline2/src/rpgonline/part/TextureParticle.java
+++ RPGOnline2/src/rpgonline/part/TextureParticle.java
@@ -5,13 +5,40 @@ import java.util.List;
 import rpgonline.texture.TextureMap;
 import rpgonline.world.World;
 
+/**
+ * A particle with a texture.
+ * @author Tomas
+ *
+ */
 public class TextureParticle implements Particle {
+	/**
+	 * The X position of the particle.
+	 */
 	protected float x;
+	/**
+	 * The Y position of the particle.
+	 */
 	protected float y;
+	/**
+	 * The texture of the particle.
+	 */
 	protected int t;
+	/**
+	 * The time the particle has left on the screen.
+	 */
 	protected float time;
+	/**
+	 * The maximum time of this particle.
+	 */
 	private final float otime;
 	
+	/**
+	 * Constructs a new TextureParticle.
+	 * @param s The texture of the particle.
+	 * @param x The X position of the particle.
+	 * @param y The Y position of the particle.
+	 * @param time The time the particle has left on the screen.
+	 */
 	public TextureParticle(String s, float x, float y, float time) {
 		this.t = TextureMap.getTextureIndex(s);
 		this.x = x;
@@ -20,21 +47,33 @@ public class TextureParticle implements Particle {
 		this.otime = time;
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public int getTexture() {
 		return t;
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public float getX() {
 		return x;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public float getY() {
 		return y;
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void doBehaviour(World w, float wind, List<Particle> particles, float delta) {
 		time -= delta;
@@ -47,6 +86,9 @@ public class TextureParticle implements Particle {
 		x += wind * delta;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public float getAlpha() {
 		if (time > otime - 1f) {
