890e918cdfe13e176cac0581e70cf966e81c76e5
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2019-09-18 21:25:23 +0100

    Updated version number.

--- RPGOnline2/src/io/github/tomaso2468/rpgonline/RPGOnline.java
+++ RPGOnline2/src/io/github/tomaso2468/rpgonline/RPGOnline.java
@@ -54,7 +54,7 @@ public final class RPGOnline {
 	/**
 	 * The current version of the RPGOnline library.
 	 */
-	public static final Version VERSION = new Version("0.8.0-d3+1568827860");
+	public static final Version VERSION = new Version("0.8.0-d4+1568838312");
 	/**
 	 * The version of java that this library was compiled with.
 	 */
