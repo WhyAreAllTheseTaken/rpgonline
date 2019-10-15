c20ec831d51ae57c27ca718d206cb25de2c6e2e7
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2019-10-15 19:46:47 +0100

    Added blank transition.

--- RPGOnline2/src/io/github/tomaso2468/rpgonline/transition/BlankTransition.java
+++ RPGOnline2/src/io/github/tomaso2468/rpgonline/transition/BlankTransition.java
@@ -1,5 +1,23 @@
 package io.github.tomaso2468.rpgonline.transition;
 
+import io.github.tomaso2468.rpgonline.Game;
+import io.github.tomaso2468.rpgonline.GameState;
+import io.github.tomaso2468.rpgonline.render.Renderer;
+
 public class BlankTransition implements Transition {
+	@Override
+	public void update(Game game, GameState currentState, GameState nextState, float delta) {
+		
+	}
+
+	@Override
+	public void render(Game game, GameState currentState, GameState nextState, Renderer renderer) {
+		
+	}
+
+	@Override
+	public boolean isDone() {
+		return true;
+	}
 
 }
