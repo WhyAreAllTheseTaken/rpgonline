3292a498c0e46c5733ef92f1a81ff9a4cbc495a8
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2019-10-15 19:38:22 +0100

    Added transition interface.

--- RPGOnline2/src/io/github/tomaso2468/rpgonline/transition/Transition.java
+++ RPGOnline2/src/io/github/tomaso2468/rpgonline/transition/Transition.java
@@ -1,5 +1,12 @@
 package io.github.tomaso2468.rpgonline.transition;
 
-public interface Transition {
+import io.github.tomaso2468.rpgonline.Game;
+import io.github.tomaso2468.rpgonline.GameState;
+import io.github.tomaso2468.rpgonline.render.Renderer;
 
+public interface Transition {
+	public void start(Game game, GameState currentState, GameState nextState);
+	public void update(Game game, GameState currentState, GameState nextState, float delta);
+	public void render(Game game, GameState currentState, GameState nextState, Renderer renderer);
+	public boolean isDone();
 }
