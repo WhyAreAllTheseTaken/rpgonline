2cb9f0a31a088ae9596f4589891b7fbbbdebc5b8
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2019-11-13 20:29:52 +0000

    Documented GameState

--- RPGOnline2/src/io/github/tomaso2468/rpgonline/GameState.java
+++ RPGOnline2/src/io/github/tomaso2468/rpgonline/GameState.java
@@ -35,35 +35,96 @@ import io.github.tomaso2468.rpgonline.render.Graphics;
 import io.github.tomaso2468.rpgonline.render.RenderException;
 import io.github.tomaso2468.rpgonline.render.Renderer;
 
+/**
+ * A single state in a game.
+ * 
+ * @author Tomaso2468
+ *
+ */
 public interface GameState {
+	/**
+	 * Called when a state is entered.
+	 * 
+	 * @param game The game.
+	 * @throws RenderException If an error occurs involving the rendering engine.
+	 */
 	public default void enterState(Game game) throws RenderException {
-		
-	}
-	
-	public default void exitState(Game game) throws RenderException {
-		
-	}
-	
-	public default void init(Game game) throws RenderException {
-		
+
 	}
 
+	/**
+	 * Called when a state is exited.
+	 * 
+	 * @param game The game.
+	 * @throws RenderException If an error occurs involving the rendering engine.
+	 */
+	public default void exitState(Game game) throws RenderException {
+
+	}
+
+	/**
+	 * Initialises the game state. This is called after all states have been added
+	 * before the init method completes. If your game creates states after the game
+	 * has inited, your game should call this method. Optionally, lazy loading can
+	 * be used an initialisation can be done in the enterState() method.
+	 * 
+	 * @param game The game
+	 * @throws RenderException If an error occurs involving the rendering engine.
+	 * 
+	 * @see #enterState(Game)
+	 */
+	public default void init(Game game) throws RenderException {
+
+	}
+
+	/**
+	 * Called every game update to update the state.
+	 * @param game The game.
+	 * @param delta The time since the last update in seconds.
+	 * @throws RenderException If an error occurs involving the rendering engine.
+	 */
 	public default void update(Game game, float delta) throws RenderException {
 
 	}
-	
+
+	/**
+	 * Called to render the state.
+	 * @param game The game.
+	 * @param renderer The renderer.
+	 * @throws RenderException If an error occurs involving the rendering engine.
+	 */
 	public void render(Game game, Renderer renderer) throws RenderException;
-	
+
+	/**
+	 * Gets the unique ID number of this state.
+	 * @return An integer value.
+	 */
 	public int getID();
-	
+
+	/**
+	 * Draw the left half of this state's debug information.
+	 * @param game The game.
+	 * @param g The GUI graphics context.
+	 * @param y The y position of the text.
+	 * @return The next available y position for rendering text.
+	 * @throws RenderException If an error occurs involving the rendering engine.
+	 */
 	public default float drawDebugLeft(Game game, Graphics g, float y) throws RenderException {
 		y = game.drawDebugLine(g, "N/A", y, false);
 		return y;
 	}
-	
+
+	/**
+	 * Draw the right half of this state's debug information.
+	 * @param game The game.
+	 * @param g The GUI graphics context.
+	 * @param y The y position of the text.
+	 * @return The next available y position for rendering text.
+	 * @throws RenderException If an error occurs involving the rendering engine.
+	 */
 	public default float drawDebugRight(Game game, Graphics g, float y) throws RenderException {
 		y = game.drawDebugLine(g, "N/A", y, true);
 		return y;
 	}
-	
+
 }
